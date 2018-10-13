34803ec99effac2d06c6ee8c8435cfe16e4f73a6
kekoasantana <34821433+kekoasantana@users.noreply.github.com>
2018-10-12 20:04:56 -1000

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+#include "mathOperations.h"
+
+int main(int argc, char** argv) {
+	int a = 6;
+	int b = 3;
+	printf("%lf\n", add(a, b));
+	printf("%lf\n", subtract(a, b));
+}
